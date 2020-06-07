1035161143c221fb33e4e230807d6acb2a1e4003
Vsevolod <vsevolod628@gmail.com>
2020-06-07 12:30:47 +0300

    заходит без логина и пароля, для удобства тестов

--- src/Commands/Utils/Readers/PrimitiveAndReferenceReaders/LoginPassReader.java
+++ src/Commands/Utils/Readers/PrimitiveAndReferenceReaders/LoginPassReader.java
@@ -13,29 +13,29 @@ public class LoginPassReader {
     }
 
     public String[] tryAuthOrRegistration() throws InterruptedException, IOException, ClassNotFoundException {
-        String login = "";
-        String password = "";
+        String login = "111";
+        String password = "111";
 
-        Console console = System.console();
-
-        String type = console.readLine("Приветствуем! Вы уже зарегистрированы? [Да/Нет] ").trim().toUpperCase();
-        if (type.equals("ДА")) {
-            login = console.readLine("Введите логин: ");
-            char[] passwordArray = console.readPassword("Введите Ваш очень нужный пароль: ");
-            password = new String(passwordArray);
-
-        } else if (type.equals("НЕТ")) {
-            login = console.readLine("Введите логин: ");
-            char[] passwordArray = console.readPassword("Введите Ваш очень нужный пароль: ");
-            char[] passwordArrayCheck;
-
-            do {
-                passwordArrayCheck = console.readPassword("Введите Ваш очень нужный пароль еще раз: ");
-                password = new String(passwordArray);
-            } while (!Arrays.equals(passwordArray, passwordArrayCheck));
-
-            commandReceiver.register(login, password);
-        } else { System.out.println("Такого варианта нет. В следующий раз будь осторожнее. Пока!"); System.exit(0);}
+//        Console console = System.console();
+//
+//        String type = console.readLine("Приветствуем! Вы уже зарегистрированы? [Да/Нет] ").trim().toUpperCase();
+//        if (type.equals("ДА")) {
+//            login = console.readLine("Введите логин: ");
+//            char[] passwordArray = console.readPassword("Введите Ваш очень нужный пароль: ");
+//            password = new String(passwordArray);
+//
+//        } else if (type.equals("НЕТ")) {
+//            login = console.readLine("Введите логин: ");
+//            char[] passwordArray = console.readPassword("Введите Ваш очень нужный пароль: ");
+//            char[] passwordArrayCheck;
+//
+//            do {
+//                passwordArrayCheck = console.readPassword("Введите Ваш очень нужный пароль еще раз: ");
+//                password = new String(passwordArray);
+//            } while (!Arrays.equals(passwordArray, passwordArrayCheck));
+//
+//            commandReceiver.register(login, password);
+//        } else { System.out.println("Такого варианта нет. В следующий раз будь осторожнее. Пока!"); System.exit(0);}
 
         return new String[] {login, password};
     }
