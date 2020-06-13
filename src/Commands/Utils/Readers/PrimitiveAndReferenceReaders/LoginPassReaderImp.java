4fba4f091b1ba5e122ed87579e2a31ca8fe02f20
Aurelius <mraurelius@yandex.ru>
2020-06-13 14:49:15 +0300

    Reader of login and password

--- src/Commands/Utils/Readers/PrimitiveAndReferenceReaders/LoginPassReaderImp.java
+++ src/Commands/Utils/Readers/PrimitiveAndReferenceReaders/LoginPassReaderImp.java
@@ -20,33 +20,33 @@ public class LoginPassReaderImp implements LoginPassReader {
 
     @Override
     public String[] tryAuthOrRegistration() throws InterruptedException, IOException, ClassNotFoundException {
-        String login = "111";
-        String password = hashEncrypter.encryptString("111");
+        String login = "";
+        String password = "";
 
-//        Console console = System.console();
-//        if (console == null){
-//            System.out.println("Запускайте из консоли");
-//            System.exit(0);
-//        }
-//
-//        String type = console.readLine("Приветствуем! Вы уже зарегистрированы? [Да/Нет] ").trim().toUpperCase();
-//        if (type.equals("ДА")) {
-//            login = console.readLine("Введите логин: ");
-//            char[] passwordArray = console.readPassword("Введите Ваш очень нужный пароль: ");
-//            password = hashEncrypter.encryptString(new String(passwordArray));
-//
-//        } else if (type.equals("НЕТ")) {
-//            login = console.readLine("Введите логин: ");
-//            char[] passwordArray = console.readPassword("Введите Ваш очень нужный пароль: ");
-//            char[] passwordArrayCheck;
-//
-//            do {
-//                passwordArrayCheck = console.readPassword("Введите Ваш очень нужный пароль еще раз: ");
-//                password = hashEncrypter.encryptString(new String(passwordArray));
-//            } while (!Arrays.equals(passwordArray, passwordArrayCheck));
-//
-//            commandReceiver.register(login, password);
-//        } else { System.out.println("Такого варианта нет. В следующий раз будь осторожнее. Пока!"); System.exit(0);}
+        Console console = System.console();
+        if (console == null){
+            System.out.println("Запустите клиент из jar файла.");
+            System.exit(0);
+        }
+
+        String type = console.readLine("Приветствуем! Вы уже зарегистрированы? [Да/Нет] ").trim().toUpperCase();
+        if (type.equals("ДА")) {
+            login = console.readLine("Введите логин: ");
+            char[] passwordArray = console.readPassword("Введите Ваш очень нужный пароль: ");
+            password = hashEncrypter.encryptString(new String(passwordArray));
+
+        } else if (type.equals("НЕТ")) {
+            login = console.readLine("Введите логин: ");
+            char[] passwordArray = console.readPassword("Введите Ваш очень нужный пароль: ");
+            char[] passwordArrayCheck;
+
+            do {
+                passwordArrayCheck = console.readPassword("Введите Ваш очень нужный пароль еще раз: ");
+                password = hashEncrypter.encryptString(new String(passwordArray));
+            } while (!Arrays.equals(passwordArray, passwordArrayCheck));
+
+            commandReceiver.register(login, password);
+        } else { System.out.println("Такого варианта нет. В следующий раз будь осторожнее. Пока!"); System.exit(0);}
 
         return new String[] {login, password};
     }
